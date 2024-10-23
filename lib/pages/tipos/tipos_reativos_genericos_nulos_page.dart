bf0d8dbb2336cd20fb2bfc01148f14bf24fb4916
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-10-23 15:26:12 -0300

    tipo nulos Preferencialmente dever usar Rx'n'

--- lib/pages/tipos/tipos_reativos_genericos_nulos_page.dart
+++ lib/pages/tipos/tipos_reativos_genericos_nulos_page.dart
@@ -3,66 +3,18 @@ import 'package:get/get.dart';
 import 'package:get_state_management/pages/tipos/model/aluno.dart';
 
 class TiposReativosGenericosNuloPage extends StatelessWidget {
-  // IGUAL A // final nome = 'Dario'.obs;
-  // final nome = RxString('Dario');
-  //! >>>>>>>>>>>GENERICO<<<<<<<<<<<<<<<<<<<<<<<<<
-  final nome = Rx<String>('Dario');
-
-  // IGUAL A // final isAluno = true.obs;
-  // final isAluno = RxBool(true);
-  //! >>>>>>>>>>>GENERICO<<<<<<<<<<<<<<<<<<<<<<<<<
-  final isAluno = Rx<bool>(true);
-
-  // IGUAL A // final qtdCurso = 1.obs;
-  // final qtdCurso = RxInt(1);
-  //! >>>>>>>>>>>GENERICO<<<<<<<<<<<<<<<<<<<<<<<<<
-  final qtdCurso = Rx<int>(1);
-
-  // IGUAL A // final valorCurso = 1250.obs;
-  // final valorCurso = RxDouble(1250);
-  //! >>>>>>>>>>>GENERICO<<<<<<<<<<<<<<<<<<<<<<<<<
-  final valorCurso = Rx<double>(1250);
-
-  // IGUAL A // final jornadas = [
-  //   'Jornada Dart',
-  //   'Jornada Flutter',
-  //   'Jornada GetX',
-  //   'Jornada BackEnd',
-  // ].obs;
-  // final jornadas = RxList([
-  //   'Jornada Dart',
-  //   'Jornada Flutter',
-  //   'Jornada GetX',
-  //   'Jornada BackEnd',
-  // ]);
-  //! >>>>>>>>>>>GENERICO<<<<<<<<<<<<<<<<<<<<<<<<<
-  final jornadas = Rx<List<String>>([
-    'Jornada Dart',
-    'Jornada Flutter',
-    'Jornada GetX',
-    'Jornada BackEnd',
-  ]);
-
-  // IGUAL A //final aluno = {
-  //   'id': 0001,
-  //   'nasc': 1984,
-  //   'nome': 'Dario',
-  //   'sexo': 'MASC',
-  // }.obs;
-  // final aluno = RxMap({
-  //   'id': 0001,
-  //   'nome': 'Dario',
-  //   'nasc': 1984,
-  //   'sexo': 'MASC',
-  // });
-//! >>>>>>>>>>>GENERICO<<<<<<<<<<<<<<<<<<<<<<<<< ADD TIPO DE VARIAVEL DO MAPA
-  final aluno = Rx<Map<String, dynamic>>({
+  //! >>>>>>>>>>>GENERICO NULO<<<<<<<<<<<<<<<<<<<<<<<<< Rxn = R. X. Nulable
+  final nome = Rxn<String>('Dario');
+  final isAluno = Rxn<bool>(true);
+  final qtdCurso = Rxn<int>(1);
+  final valorCurso = Rxn<double>(1250);
+  final jornadas = Rxn<List<String>>(['Jornada Dart', 'Jornada Flutter']);
+  final aluno = Rxn<Map<String, dynamic>>({
     'id': 0001,
     'nome': 'Dario',
     'nasc': 1984,
     'sexo': 'MASC',
   });
-//! GERANDO MEU PROPRIO RX - alunoModel Observavel
   final alunoModel = Rx<Aluno>(
     Aluno(
       id: 1999,
@@ -78,7 +30,10 @@ class TiposReativosGenericosNuloPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tipos Reativos Genericos NULOS Page', style: TextStyle(fontSize: 16),),
+        title: const Text(
+          'Tipos Reativos Genericos NULOS Page',
+          style: TextStyle(fontSize: 16),
+        ),
       ),
       body: Center(
         child: Column(
@@ -89,10 +44,10 @@ class TiposReativosGenericosNuloPage extends StatelessWidget {
             Obx(() {
               debugPrint('>>> Montando ID do aluno');
               //!>>>>>>>>>>>>>>> RECUPERAÇÃO DO GENERICO
-              return Text('Id do aluno ${aluno.value['id']}');
+              return Text('Id do aluno ${aluno.value?['id']}');
             }),
             //!>>>>>>>>>>>>>>> RECUPERAÇÃO DO GENERICO
-            Obx(() => Text('Nome do aluno ${aluno.value['nome']}')),
+            Obx(() => Text('Nome do aluno ${aluno.value?['nome']}')),
 
             Obx(() {
               debugPrint('>>> Montando ID do alunoModel');
@@ -106,12 +61,12 @@ class TiposReativosGenericosNuloPage extends StatelessWidget {
             //!>>>>>>>>>>>>>>> RECUPERAÇÃO DO GENERICO
             Obx(
               () => Column(
-                children: jornadas.value.map(Text.new).toList(),
+                children: jornadas.value?.map(Text.new).toList() ?? [],
               ),
             ),
             ElevatedButton(
               onPressed: () {
-                aluno.value['id'] = 10;
+                aluno.value?['id'] = 10;
                 //! NECESSÁRIO REFRESH PARA ATUALIZAR
                 aluno.refresh();
               },
@@ -121,7 +76,7 @@ class TiposReativosGenericosNuloPage extends StatelessWidget {
               onPressed: () {
                 // jornadas.add('Jornada Lógica');
                 //!ZERA a LISTA e add o ultomo item 'Jornada Lógica'
-                jornadas.value.assign('Jornada Lógica');
+                jornadas.value?.assign('Jornada Lógica');
                 //! NECESSÁRIO REFRESH PARA ATUALIZAR
                 jornadas.refresh();
               },
